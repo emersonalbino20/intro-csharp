07d335bade076c5b849ccb37581d1d86c8b3c563
Emerson Joaquim Tito Albino <ealbino@c2r9s3.42luanda.com>
2025-11-27 18:13:15 +0100

    feat: add 2DArray

---
+++ TowDArray/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TwoDArray;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[,] matrix;
+        matrix = new string[2,2]
+        {
+            { "A", "B" },
+            { "C", "D" },
+        };
+        
+        int[,] array2D = new int[,]
+        {
+            { 1, 2, 3 }, 
+            { 4, 5, 6 }, 
+            { 7, 8, 9 } 
+        };
+
+        int[,] array2D2 = {{ 1, 2 },{ 3, 4 }}; 
+        
+        Console.WriteLine($"Central value is {array2D[1, 1]}");
+    }
+}
