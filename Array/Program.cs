cb5af843d7ad8aa11f66e03b1e4f4108b5f7f301
Emerson Joaquim Tito Albino <ealbino@c2r9s3.42luanda.com>
2025-11-27 17:32:14 +0100

    feat: add Array

---
+++ Array/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Array;
+
+class Program
+{
+    private static int max = 0;
+
+    private static int Max
+    {
+        get => max;
+        set => max = value;
+    }
+
+    private static void FillArray(int[] array)
+    {
+        string input;
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine($"Tell the grade number {i}");
+            input = Console.ReadLine();
+            if (!int.TryParse(input, out array[i]))
+                array[i] = 0;
+            if (array[i] > Max)
+                Max = array[i];
+        }
+    }
+    
+    static void Main(string[] args)
+    {
+        int[] grades = new int[5];
+        FillArray(grades);        
+        for (int i = 0; i < grades.Length; i++)
+            Console.WriteLine($"grades at index {i} : {grades[i]}");
+        Console.WriteLine($"Major grade is {Max}");
+    }
+}
